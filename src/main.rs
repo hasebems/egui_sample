73a7f5fe2d2830fb0e181fd98304e43fd17809e0
HASEBE Masahiko <JCA03205@gmail.com>
2023-10-28 22:19:32 +0900

    常に波が出るパターン作成

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,81 @@
-use eframe::{egui::*};
+use eframe::egui::*;
+use std::time::{Duration, Instant};
 
-#[derive(Default)]
-pub struct EguiSample {}
+pub struct NoteObj {
+    para1: f32,
+    para2: f32,
+    para3: f32,
+    time: i32,
+}
+
+impl NoteObj {
+    fn disp(&self, crnt_time: i32, ui: &mut Ui) {
+        let cnt = crnt_time - self.time;
+        for i in 0..256 {
+            let position = 127-(cnt*4)%128;
+            let phase = std::f32::consts::PI*((i+position) as f32)/32.0;  // 波紋の密度
+            let gray = 255.0 - (phase.sin()*255.0).abs();               // 濃淡の関数(sinの絶対値)
+            let gray_scl = (gray*(255.0-(i as f32))/511.0) as u8;        // 白/Alpha値への変換
+            ui.painter().circle_stroke(
+                Pos2 {x:self.para1, y:self.para2},  // location
+                i as f32,                                   // radius
+                Stroke {width:1.0, color:Color32::from_white_alpha(gray_scl)}
+            );
+        }
+    }
+}
+
+pub struct EguiSample {
+    cnt: i32,
+    instant: Instant,
+    pos: Pos2,
+    nobj: Vec<NoteObj>,
+}
 
 impl EguiSample {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let mut fonts = FontDefinitions::default();
-
-        fonts.font_data.insert(
-            "profont".to_owned(),
-            FontData::from_static(include_bytes!("../assets/newyork.ttf")),
-        );
-        fonts.font_data.insert(
-            "monofont".to_owned(),
-            FontData::from_static(include_bytes!("../assets/courier.ttc")),
-        );
-
-        fonts
-            .families
-            .entry(FontFamily::Proportional)    //  search value of this key
-            .or_default()                       //  if not found
-            .insert(0, "profont".to_owned());
-        fonts
-            .families
-            .entry(FontFamily::Monospace)
-            .or_default()
-            .insert(0, "monofont".to_owned());
-
-        cc.egui_ctx.set_fonts(fonts);
-        Self::default()
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self {
+            cnt: 0,
+            instant: Instant::now(),
+            pos: Pos2 {x:400.0, y:400.0},
+            nobj: vec![NoteObj {para1:200.0,para2:200.0,para3:0.0,time:0}],
+        }
     }
 }
 
 impl eframe::App for EguiSample {
-    fn save(&mut self, _storage: &mut dyn eframe::Storage) {}       
-    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+    //fn save(&mut self, _storage: &mut dyn eframe::Storage) {}       
+    fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
+        ctx.request_repaint_after(Duration::from_millis(25));
+        if self.instant.elapsed() >= Duration::from_millis(50) {
+            self.cnt += 1;
+            self.instant = Instant::now();
+        }
+
         CentralPanel::default().show(ctx, |ui| {
-            ui.painter().vline(
-                200.0,                                        // x
-                std::ops::RangeInclusive::new(100.0, 300.0),  // y
-                Stroke {width:3.0, color:Color32::RED},       // width, color
-            );
+            for obj in self.nobj.iter() {
+                obj.disp(self.cnt, ui);
+            }
 
-            ui.painter().rect_filled(
-                Rect { min: Pos2 {x:50.0, y:50.0}, 
-                       max: Pos2 {x:150.0,y:150.0},},   // location
-                8.0,                                    // curve
-                Color32::from_rgb(199,21,133),          // color
-            );
-
-            ui.painter().circle_filled(
-                Pos2 {x:300.0, y:300.0},  // location
-                50.0,                     // radius
-                Color32::GREEN,           // color
-            );
-
-            ui.painter().text(
-                Pos2 {x:100.0, y:300.0},
-                Align2::CENTER_CENTER,
-                "Hello,",
-                FontId::new(36.0, FontFamily::Proportional),
-                Color32::WHITE
-            );
-            ui.painter().text(
-                Pos2 {x:300.0, y:100.0},
-                Align2::CENTER_CENTER,
-                "World!",
-                FontId::new(24.0, FontFamily::Monospace),
-                Color32::BLUE
-            );
+            if self.cnt%10 == 0 {
+                //frame.set_window_size(Vec2{x:200.0,y:400.0});
+                self.pos.x = frame.info().window_info.size.x;
+                self.pos.y = frame.info().window_info.size.y;
+                println!("x:{},y:{}", self.pos.x, self.pos.y);
+            }
         });
     }
 }
 
 fn main() {
-    let options = eframe::NativeOptions {
+    let native_options = eframe::NativeOptions {
         initial_window_size: Some((400.0, 400.0).into()),
-        resizable: false,
+        //resizable: false,
         ..eframe::NativeOptions::default()
     };
-    eframe::run_native("egui_sample", options, Box::new(|cc| Box::new(EguiSample::new(cc))));
+    let _ = eframe::run_native(
+        "egui_sample",
+        native_options,
+        Box::new(|cc| Box::new(EguiSample::new(cc)))
+    );
 }
